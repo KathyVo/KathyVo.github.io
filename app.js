e3c3ae4fcfddbc405d2a838c9034f15910fcd9e1
Kathy Vo <kathyvo143@gmail.com>
2016-02-01 20:26:42 -0800

    categorize projects

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
 $(document).ready(function() {
 
-    $("#ri").click(function() {
-    $(".active").fadeOut(300, function() {
-            $("#raidin").removeClass("hide");
-         	$("#raidin").fadeIn(300);
-         });
+     $("#projects").click(function() { 
+        $(".active").fadeOut(300, function() { 
+            $("#portfolio").removeClass("hidden"); 
+            $("#portfolio").fadeIn(300);
+        })
         $(".active").removeClass("active"); 
-        $("#raidin").addClass("active");
-    })
-    
-     $("#abt").click(function() {
-    
-        $(".active").fadeOut(300, function() {
-            $("#about").removeClass("hide");
-         	$("#about").fadeIn(300);
-         });
+        $("#portfolio").addClass("active");
+     });
+
+      $("#abt").click(function() { 
+        $(".active").fadeOut(300, function() { 
+            $("#about").removeClass("hidden"); 
+            $("#about").fadeIn(300);
+        })
         $(".active").removeClass("active"); 
         $("#about").addClass("active");
-     })
-     
-     $("#lo").click(function() {
-    
-         $(".active").fadeOut(300, function() {
-            $("#liftoff").removeClass("hide");
-         	$("#liftoff").fadeIn(300);
-         });
+     });
+
+      /*$("#res").click(function() { 
+        $(".active").fadeOut(300, function() { 
+            $("#resume").removeClass("hidden"); 
+            $("#resume").fadeIn(300);
+        })
         $(".active").removeClass("active"); 
-        $("#liftoff").addClass("active");
-    })
-     
-     $("#eye").click(function() {
-    
-    	$(".active").fadeOut(300, function() {
-            $("#eyewitness").removeClass("hide");
-         	$("#eyewitness").fadeIn(300);
-         });
+        $("#resume").addClass("active");
+     });*/
+
+      $("#ri").click(function() { 
+        $(".active").fadeOut(300, function() { 
+            $("#raidin").removeClass("hidden"); 
+            $("#raidin").fadeIn(300);
+        })
+        $(".active").removeClass("active"); 
+        $("#raidin").addClass("active");
+     });
+
+        $("#vets").click(function() { 
+        $(".active").fadeOut(300, function() { 
+            $("#ptci").removeClass("hidden"); 
+            $("#ptci").fadeIn(300);
+        })
+        $(".active").removeClass("active"); 
+        $("#ptci").addClass("active");
+     });
+
+        $("#eye").click(function() { 
+        $(".active").fadeOut(300, function() { 
+            $("#eyewitness").removeClass("hidden"); 
+            $("#eyewitness").fadeIn(300);
+        })
         $(".active").removeClass("active"); 
         $("#eyewitness").addClass("active");
-    })
-     
-    $("#cont").click(function() {
-    
-    	$("#contact").removeClass("hide");
-         $(".active").fadeOut(300, function() {
-         	$("#contact").fadeIn(300);
-         });
-        $(".active").removeClass("active"); 
-        $("#contact").addClass("active");
-    })
-
+     });
 })
---
+++ proj.js
@@ -0,0 +1,5 @@
+$(document).ready(function() { 
+
+	$("#portfolio").fadeIn(500);
+	
+})
